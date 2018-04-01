83b65ab179d09c81c07ab0c8a851fe08e59d3692
Marco Burstein <theskunkmb@gmail.com>
2018-04-01 10:17:20 -0700

    Test `Config.init`.
    Test that the
     - `emailAddress`,
     - `apiKey`, and
     - `apiURL`
    properties are changed during `init`.

---
+++ tests/ZulipSwift/config.swift
@@ -0,0 +1,28 @@
+import XCTest
+@testable import ZulipSwift
+
+class ConfigTests: XCTestCase {
+    func testInit() {
+        let config = Config(
+            emailAddress: "theskunkmb@gmail.com",
+            apiKey: "qwertyuiop",
+            realmURL: "http://example.com"
+        );
+
+        XCTAssertEqual(
+            config.emailAddress,
+            "theskunkmb@gmail.com",
+            "`Config.emailAddress` is incorrect."
+        )
+        XCTAssertEqual(
+            config.apiKey,
+            "qwertyuiop",
+            "`Config.apiKey` is incorrect."
+        )
+        XCTAssertEqual(
+            config.apiURL,
+            "http://example.com/api/v1",
+            "`Config.apiURL` is incorrect."
+        )
+    }
+}
