6864a9171a90bea610bf1b8a2c7a1bedb1eb6a74
Marco Burstein <theskunkmb@gmail.com>
2018-04-01 15:34:08 -0700

    Create the `send` method of `Messages`.
    It uses `makePostRequest` to send a message to either a Zulip stream or
    send a private message.

--- sources/ZulipSwift/messages.swift
+++ sources/ZulipSwift/messages.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 //: A type of Zulip message.
 public enum MessageType: String {
     //: A stream message.
@@ -21,5 +23,46 @@ public class Messages {
      */
     init(config: Config) {
         self.config = config
+    }
+
+    /*
+        Sends a message.
+
+         - Parameters:
+            - messageType: The type of message to send.
+            - to: If `messageType` is `MessageType.stream`, then a string
+              identifying a stream. If `messageType` is `MessageType.private`,
+              then list of users' email addresses to send messages to.
+               - Example: `StreamName`
+               - Example: `user@example.com`
+               - Example: `user1@example.com,user2@example.org`
+            - subject: The subject of the message.
+            - content: The content of the message, which will be formatted by
+              Zulip's Markdown engine on the backend.
+     */
+    func send(
+        messageType: MessageType,
+        to: String,
+        subject: String?,
+        content: String,
+        callback: @escaping (Dictionary<String, Any>?) -> Void
+    ) {
+        var params = [
+            "type": messageType.rawValue,
+            "to": to,
+            "content": content,
+        ]
+
+        if messageType == MessageType.streamMessage, let subject = subject {
+            params["subject"] = subject
+        }
+
+        makePostRequest(
+            url: self.config.apiURL + "/messages",
+            params: params,
+            username: config.emailAddress,
+            password: config.apiKey,
+            callback: callback
+        )
     }
 }
