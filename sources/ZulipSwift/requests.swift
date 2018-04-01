f0a59e566c24c044fe8e08238e196b2d83b02ba9
Marco Burstein <theskunkmb@gmail.com>
2018-04-01 16:34:09 -0700

    Create the `makeGetRequest` function.

--- sources/ZulipSwift/requests.swift
+++ sources/ZulipSwift/requests.swift
@@ -38,3 +38,40 @@ internal func makePostRequest(
         request.responseJSON(completionHandler: completionHandler)
     }
 }
+
+/*:
+    Makes an HTTP GET request.
+
+     - Parameters:
+        - urlString: The URL to make the request to.
+        - params: The dictionary of parameters for the POST request's body.
+        - username: A username for authentication, if necessary.
+        - password: A password for authentication, if necessary.
+        - callback: The callback to pass the response to.
+ */
+internal func makeGetRequest(
+    url: String,
+    params: [String: String],
+    username: String?,
+    password: String?,
+    callback: @escaping (DataResponse<Any>) -> Void
+) {
+    let request = Alamofire.request(
+        url,
+        method: HTTPMethod.get,
+        parameters: params
+    )
+
+    let completionHandler = { (response: DataResponse<Any>) in
+        callback(response)
+    }
+
+    if let username = username, let password = password {
+        request.authenticate(
+            user: username,
+            password: password
+        ).responseJSON(completionHandler: completionHandler)
+    } else {
+        request.responseJSON(completionHandler: completionHandler)
+    }
+}
