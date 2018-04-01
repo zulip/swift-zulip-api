564ca553c9bd66e8ee789a243519875c6e12e29b
Marco Burstein <theskunkmb@gmail.com>
2018-04-01 09:26:04 -0700

    Create the `Config` class.
    `Config` will be used to initialize Zulip.

---
+++ sources/ZulipSwift/config.swift
@@ -0,0 +1,27 @@
+/*:
+    A Zulip configuration that must be created before using the Zulip API.
+ */
+public class Config {
+    public var emailAddress: String
+    public var apiKey: String
+    public var apiURL: String
+
+    /*
+        Initializes a Zulip configuration.
+
+        - Parameters:
+           - emailAddress: An email address that is signed up for Zulip.
+           - apiKey: An API key for a Zulip user.
+           - realmURL: The URL of a Zulip realm.
+             - Example: https://chat.zulip.org
+     */
+    init(
+        emailAddress: String,
+        apiKey: String,
+        realmURL: String,
+    ) {
+        self.emailAddress = emailAddress
+        self.apiKey = apiKey
+        self.apiURL = realmURL + "/api/v1"
+    }
+}
