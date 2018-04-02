f894fcb6ceeab8c751cec50312fc26c4d8d0f86d
Marco Burstein <theskunkmb@gmail.com>
2018-04-02 08:18:06 -0700

    Remove `getZulip` from the `messages.swift` tests.
    Now that `getZulip` is defined in `setup.swift`, there is no need to
    have a duplicate of it.

--- tests/ZulipSwift/messages.swift
+++ tests/ZulipSwift/messages.swift
@@ -22,33 +22,4 @@ class MessagesTests: XCTestCase {
 
         wait(for: expectations, timeout: 60)
     }
-
-    func getZulip() -> Zulip? {
-        let environmentVariables = ProcessInfo.processInfo.environment
-
-        guard
-            let testEmailAddress = environmentVariables["emailAddress"]
-        else {
-            XCTFail("No `emailAddress` was set.")
-            return nil
-        }
-
-        guard let testAPIKey = environmentVariables["apiKey"] else {
-            XCTFail("No `apiKey` was set.")
-            return nil
-        }
-
-        guard let testRealmURL = environmentVariables["realmURL"] else {
-            XCTFail("No `realmURL` was set.")
-            return nil
-        }
-
-        let config = Config(
-            emailAddress: testEmailAddress,
-            apiKey: testAPIKey,
-            realmURL: testRealmURL
-        )
-
-        return Zulip(config: config)
-    }
 }
