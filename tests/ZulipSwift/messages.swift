eb688bdadf83c4f3bc827a985fa732bffc4614b6
Marco Burstein <theskunkmb@gmail.com>
2018-04-02 08:19:36 -0700

    Move `testSend` above `testGet`.
    `Messages.send` is defined above `Messages.get` in `messages.swift`,
    so it makes sense to test it first, too. Furthermore, in theory, someone
    has to send a message before someone else can receive it.

--- tests/ZulipSwift/messages.swift
+++ tests/ZulipSwift/messages.swift
@@ -2,27 +2,6 @@ import XCTest
 @testable import ZulipSwift
 
 class MessagesTests: XCTestCase {
-    func testGet() throws {
-        guard let zulip = getZulip() else {
-            XCTFail("Zulip could not be configured.")
-            return
-        }
-
-        let expectations = [expectation(description: "`Messages.get`")]
-
-        try zulip.messages().get(
-            narrow: [["stream", "test here"]],
-            anchor: 391920,
-            amountBefore: 13,
-            amountAfter: 13,
-            callback: { (response) in
-                expectations[0].fulfill()
-            }
-        )
-
-        wait(for: expectations, timeout: 60)
-    }
-
     func testSend() {
         guard let zulip = getZulip() else {
             XCTFail("Zulip could not be configured.")
@@ -36,6 +15,27 @@ class MessagesTests: XCTestCase {
             to: "test here",
             subject: "Test Message",
             content: "Testing",
+            callback: { (response) in
+                expectations[0].fulfill()
+            }
+        )
+
+        wait(for: expectations, timeout: 60)
+    }
+
+    func testGet() throws {
+        guard let zulip = getZulip() else {
+            XCTFail("Zulip could not be configured.")
+            return
+        }
+
+        let expectations = [expectation(description: "`Messages.get`")]
+
+        try zulip.messages().get(
+            narrow: [["stream", "test here"]],
+            anchor: 391920,
+            amountBefore: 13,
+            amountAfter: 13,
             callback: { (response) in
                 expectations[0].fulfill()
             }
