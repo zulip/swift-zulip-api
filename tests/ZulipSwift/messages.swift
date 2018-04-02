506a846347497ceef98fec74d0814c82cd3eafdf
Marco Burstein <theskunkmb@gmail.com>
2018-04-02 08:19:10 -0700

    Test `Messages.send`.

--- tests/ZulipSwift/messages.swift
+++ tests/ZulipSwift/messages.swift
@@ -22,4 +22,25 @@ class MessagesTests: XCTestCase {
 
         wait(for: expectations, timeout: 60)
     }
+
+    func testSend() {
+        guard let zulip = getZulip() else {
+            XCTFail("Zulip could not be configured.")
+            return
+        }
+
+        let expectations = [expectation(description: "`Messages.send`")]
+
+        zulip.messages().send(
+            messageType: MessageType.streamMessage,
+            to: "test here",
+            subject: "Test Message",
+            content: "Testing",
+            callback: { (response) in
+                expectations[0].fulfill()
+            }
+        )
+
+        wait(for: expectations, timeout: 60)
+    }
 }
