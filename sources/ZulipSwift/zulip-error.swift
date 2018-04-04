e8553fe32c67c8240b3eaca943c42cb22232776e
Marco Burstein <theskunkmb@gmail.com>
2018-04-04 09:56:15 -0700

    Create the `ZulipError` error class.
    `ZulipError` will be used for errors that occur after making an HTTP
    response and are sent by the Zulip server. Also, create
    `getZulipErrorFromResponse` for creating `ZulipError`s.

---
+++ sources/ZulipSwift/zulip-error.swift
@@ -0,0 +1,31 @@
+import Alamofire
+
+//: An error that occurs during a Zulip HTTP request.
+public enum ZulipError: Error {
+    //: An unexpected error that occurs without a message.
+    case unknownError
+
+    //: An error that is sent by Zulip, with an accompanying message.
+    case error(message: String)
+}
+
+/*
+    Makes a `ZulipError` based on an Alamofire `DataResponse`.
+
+     - Parameters:
+        - response: An Alamofire `DataResponse` from a Zulip REST API request.
+ */
+internal func getZulipErrorFromResponse(
+    response: DataResponse<Any>
+) -> ZulipError {
+    guard
+        let errorMessage = getChildFromJSONResponse(
+            response: response,
+            childKey: "msg"
+        ) as? String
+    else {
+        return ZulipError.unknownError
+    }
+
+    return ZulipError.error(message: errorMessage)
+}
