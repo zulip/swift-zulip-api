97c1a6d7a2de86ca843392387925b9ea9f5c69b6
Marco Burstein <theskunkmb@gmail.com>
2018-04-05 13:53:57 -0700

    Create the `getAll` method of `Streams`.

--- sources/ZulipSwift/streams.swift
+++ sources/ZulipSwift/streams.swift
@@ -14,4 +14,58 @@ public class Streams {
     init(config: Config) {
         self.config = config
     }
+
+    /*
+        Gets all streams that a user can access.
+
+         - Parameters:
+            - includePublic: Whether all public streams should be included.
+            - includeSubscribed: Whether all subscribed-to streams should be
+              included
+            - includeDefault: Whether all default streams should be included.
+            - includeActive: Whether all active streams should be included.
+              This option will cause a `ZulipError` if the user not an admin.
+            - callback: A callback, which will be passed the streams, or an
+              error.
+     */
+    func getAll(
+        includePublic: Bool = true,
+        includeSubscribed: Bool = true,
+        includeDefault: Bool = false,
+        includeActive: Bool = false,
+        callback: @escaping (
+            Array<Dictionary<String, Any>>?,
+            Error?
+        ) -> Void
+    ) {
+        let params = [
+            "include_public": includePublic,
+            "include_subscribed": includeSubscribed,
+            "include_default": includeDefault,
+            "include_active": includeActive,
+        ]
+
+        makeGetRequest(
+            url: self.config.apiURL + "/streams",
+            params: params,
+            username: config.emailAddress,
+            password: config.apiKey,
+            callback: { (response) in
+                guard
+                    let streams = getChildFromJSONResponse(
+                        response: response,
+                        childKey: "streams"
+                    ) as? Array<Dictionary<String, Any>>
+                else {
+                    callback(
+                        nil,
+                        getZulipErrorFromResponse(response: response)
+                    )
+                    return
+                }
+
+                callback(streams, nil)
+            }
+        )
+    }
 }
