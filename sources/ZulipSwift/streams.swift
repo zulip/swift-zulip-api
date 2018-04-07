dfd34def5fe65e416edee9911be4fe01f67438f6
Marco Burstein <theskunkmb@gmail.com>
2018-04-06 17:37:47 -0700

    Create the `subscribe` method of `Streams`.

--- sources/ZulipSwift/streams.swift
+++ sources/ZulipSwift/streams.swift
@@ -1,6 +1,18 @@
 import Foundation
 import Alamofire
 
+/*:
+    An error that occurs during stream operations, before an HTTP request is
+    made.
+ */
+public enum StreamError: Error {
+    //: An error that occurs when a list of Zulip streams is invalid.
+    case invalidStreams
+
+    //: An error that occurs when a list of Zulip principals is invalid.
+    case invalidPrincipals
+}
+
 //: A client for interacting with Zulip's messaging functionality.
 public class Streams {
     private var config: Config
@@ -141,6 +153,112 @@ public class Streams {
                 }
 
                 callback(streams, nil)
+            }
+        )
+    }
+
+    /*:
+        Subscribes a user to streams, or creates them if they do not exist yet.
+
+         - Parameters:
+            - streams: The streams to subscribe a user to.
+               - Example: `[["name": "test here"]]`
+               - Example: `["name": "test here"], ["name": "announce"]]`
+            - inviteOnly: Whether the streams are invite only or not.
+            - announce: Whether an announcement should be made that a new
+              stream is created if it is.
+            - principals: The users to subscribe to the streams. If
+              `principals` is empty, the current user will be subscribed.
+            - authorizationErrorsFatal: Whether authorization errors should be
+              reported in the response.
+            - callback: A callback, which will be passed a dictionary where the
+              key is user's email, and the value is a list of streams they were
+              subscribed to; a dictionary where the key is a user's email, and
+              the value is a list of streams they are already subscribed to;
+              and a list of names of streams that could not be subscribed to
+              because the user was unauthorized; or an error if there is one.
+     */
+    func subscribe(
+        streams: Array<Dictionary<String, Any>>,
+        inviteOnly: Bool = false,
+        announce: Bool = false,
+        principals: [String] = [],
+        authorizationErrorsFatal: Bool = true,
+        callback: @escaping (
+            Dictionary<String, Array<String>>?,
+            Dictionary<String, Array<String>>?,
+            Array<String>?,
+            Error?
+        ) -> Void
+    ) {
+        guard
+            let streamsData = try? JSONSerialization.data(
+                withJSONObject: streams
+            ),
+            let streamsString = String(
+                data: streamsData,
+                encoding: String.Encoding.utf8
+            )
+        else {
+            callback(nil, nil, nil, StreamError.invalidStreams)
+            return
+        }
+
+        guard
+            let principalsData = try? JSONSerialization.data(
+                withJSONObject: principals
+            ),
+            let principalsString = String(
+                data: principalsData,
+                encoding: String.Encoding.utf8
+            )
+        else {
+            callback(nil, nil, nil, StreamError.invalidPrincipals)
+            return
+        }
+
+        let params = [
+            "subscriptions": streamsString,
+            "invite_only": inviteOnly ? "true" : "false",
+            "announce": announce ? "true" : "false",
+            "principals": principalsString,
+            "authorization_errors_fatal":
+                authorizationErrorsFatal ? "true" : "false",
+        ]
+
+        makePostRequest(
+            url: self.config.apiURL + "/users/me/subscriptions",
+            params: params,
+            username: config.emailAddress,
+            password: config.apiKey,
+            callback: { (response) in
+                if let errorMessage = getChildFromJSONResponse(
+                    response: response,
+                    childKey: "msg"
+                ) as? String, errorMessage != "" {
+                    callback(
+                        nil,
+                        nil,
+                        nil,
+                        ZulipError.error(message: errorMessage)
+                    )
+                    return
+                }
+
+                let subscribed = getChildFromJSONResponse(
+                    response: response,
+                    childKey: "subscribed"
+                ) as? Dictionary<String, Array<String>>
+                let alreadySubscribed = getChildFromJSONResponse(
+                    response: response,
+                    childKey: "already_subscribed"
+                ) as? Dictionary<String, Array<String>>
+                let unauthorized = getChildFromJSONResponse(
+                    response: response,
+                    childKey: "unauthorized"
+                ) as? Array<String>
+
+                callback(subscribed, alreadySubscribed, unauthorized, nil)
             }
         )
     }
