be3a1995e493cf22ac70d82f7d9977c3af09311f
Marco Burstein <theskunkmb@gmail.com>
2018-04-07 10:39:21 -0700

    Test `Users.getCurrent`.

--- tests/ZulipSwift/users.swift
+++ tests/ZulipSwift/users.swift
@@ -29,4 +29,32 @@ class UsersTests: XCTestCase {
 
         wait(for: expectations, timeout: 60)
     }
+
+    func testGetCurrent() {
+        guard let zulip = getZulip() else {
+            XCTFail("Zulip could not be configured.")
+            return
+        }
+
+        let expectations = [expectation(description: "`Users.getCurrent`")]
+
+        zulip.users().getCurrent(
+            clientGravatar: false,
+            callback: { (profile, messageError) in
+                XCTAssertNotNil(
+                    profile,
+                    "`Users.getCurrent` is not successful"
+                )
+                XCTAssertNil(
+                    messageError,
+                    "`Users.getCurrent` errors: "
+                        + String(describing: messageError)
+                )
+
+                expectations[0].fulfill()
+            }
+        )
+
+        wait(for: expectations, timeout: 60)
+    }
 }
