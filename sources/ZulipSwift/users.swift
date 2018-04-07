37933811c75572a1f6a8bdd581789121d5be734f
Marco Burstein <theskunkmb@gmail.com>
2018-04-07 10:17:32 -0700

    Create the `getAll` method of `Users`.

--- sources/ZulipSwift/users.swift
+++ sources/ZulipSwift/users.swift
@@ -16,4 +16,47 @@ public class Users {
     init(config: Config) {
         self.config = config
     }
+
+    /*:
+        Gets all user profiles in the current realm.
+
+         - Parameters:
+            - clientGravatar: Whether Gravatars should not be send if the user
+              does not have an avatar. (`true` means that `avatar_url` will be
+              `nil` if the user does not have an avatar; `false` means that an
+              `avatar_url` will be a Gravatar).
+            - callback: A callback, which will be passed a list of users, or an
+              error if there is one.
+     */
+    func getAll(
+        clientGravatar: Bool = false,
+        callback: @escaping ([[String: Any]]?, Error?) -> Void
+    ) {
+        let params = [
+            "client_gravatar": clientGravatar ? "true" : "false",
+        ]
+
+        makeGetRequest(
+            url: self.config.apiURL + "/users",
+            params: params,
+            username: config.emailAddress,
+            password: config.apiKey,
+            callback: { (response) in
+                guard
+                    let members = getChildFromJSONResponse(
+                        response: response,
+                        childKey: "members"
+                    ) as? [[String: Any]]
+                else {
+                    callback(
+                        nil,
+                        getZulipErrorFromResponse(response: response)
+                    )
+                    return
+                }
+
+                callback(members, nil)
+            }
+        )
+    }
 }
