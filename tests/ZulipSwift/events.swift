e2fe206cf9c8f7d04d960be64632d329439df1fa
Marco Burstein <theskunkmb@gmail.com>
2018-04-07 12:26:04 -0700

    Test `Events.register`.

--- tests/ZulipSwift/events.swift
+++ tests/ZulipSwift/events.swift
@@ -2,4 +2,130 @@ import XCTest
 @testable import ZulipSwift
 
 class EventsTests: XCTestCase {
+    func testRegister() {
+        guard let zulip = getZulip() else {
+            XCTFail("Zulip could not be configured.")
+            return
+        }
+
+        let expectations = [
+            expectation(description: "`Events.register`"),
+            expectation(
+                description: "`Events.register` `applyMarkdown: true`"
+            ),
+            expectation(
+                description: "`Events.register` `clientGravatar: true`"
+            ),
+            expectation(
+                description: "`Events.register` `allPublicStreams: true`"
+            ),
+            expectation(
+                description: "`Events.register` `includeSubscribers: true`"
+            ),
+        ]
+
+        zulip.events().register(
+            eventTypes: ["messages"],
+            narrow: [["stream", "test here"]],
+            callback: { (queue, error) in
+                print(queue)
+                XCTAssertNotNil(
+                    queue,
+                    "`Events.register` is not successful"
+                )
+                XCTAssertNil(
+                    error,
+                    "`Events.register` errors: "
+                        + String(describing: error)
+                )
+
+                expectations[0].fulfill()
+            }
+        )
+
+        zulip.events().register(
+            applyMarkdown: true,
+            eventTypes: ["messages"],
+            narrow: [["stream", "test here"]],
+            callback: { (queue, error) in
+                print(queue)
+                XCTAssertNotNil(
+                    queue,
+                    "`Events.register` `applyMarkdown: true` is not successful"
+                )
+                XCTAssertNil(
+                    error,
+                    "`Events.register` `applyMarkdown: true` errors: "
+                        + String(describing: error)
+                )
+
+                expectations[1].fulfill()
+            }
+        )
+
+        zulip.events().register(
+            clientGravatar: true,
+            eventTypes: ["messages"],
+            narrow: [["stream", "test here"]],
+            callback: { (queue, error) in
+                print(queue)
+                XCTAssertNotNil(
+                    queue,
+                    "`Events.register` `clientGravatar: true` is not "
+                        + "successful"
+                )
+                XCTAssertNil(
+                    error,
+                    "`Events.register` `clientGravatar: true` errors: "
+                        + String(describing: error)
+                )
+
+                expectations[2].fulfill()
+            }
+        )
+
+        zulip.events().register(
+            eventTypes: ["messages"],
+            allPublicStreams: true,
+            narrow: [["stream", "test here"]],
+            callback: { (queue, error) in
+                print(queue)
+                XCTAssertNotNil(
+                    queue,
+                    "`Events.register` `allPublicStreams: true` is not "
+                        + "successful"
+                )
+                XCTAssertNil(
+                    error,
+                    "`Events.register` `allPublicStreams: true` errors: "
+                        + String(describing: error)
+                )
+
+                expectations[3].fulfill()
+            }
+        )
+
+        zulip.events().register(
+            eventTypes: ["messages"],
+            includeSubscribers: true,
+            narrow: [["stream", "test here"]],
+            callback: { (queue, error) in
+                print(queue)
+                XCTAssertNotNil(
+                    queue,
+                    "`Events.register` `includeSubscribers: true` is not "
+                        + "successful"
+                )
+                XCTAssertNil(
+                    error,
+                    "`Events.register` `includeSubscribers: true` errors: "
+                        + String(describing: error)
+                )
+
+                expectations[4].fulfill()
+            }
+        )
+
+        wait(for: expectations, timeout: 60)
+    }
 }
