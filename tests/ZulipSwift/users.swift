21b4754fdbf3b9299fafa4a494ad2e9aa8f205a5
Marco Burstein <theskunkmb@gmail.com>
2018-04-07 10:20:42 -0700

    Test `Users.getAll`.

--- tests/ZulipSwift/users.swift
+++ tests/ZulipSwift/users.swift
@@ -2,4 +2,31 @@ import XCTest
 @testable import ZulipSwift
 
 class UsersTests: XCTestCase {
+    func testGetAll() {
+        guard let zulip = getZulip() else {
+            XCTFail("Zulip could not be configured.")
+            return
+        }
+
+        let expectations = [expectation(description: "`Users.getAll`")]
+
+        zulip.users().getAll(
+            clientGravatar: false,
+            callback: { (members, messageError) in
+                XCTAssertNotNil(
+                    members,
+                    "`Users.getAll` is not successful"
+                )
+                XCTAssertNil(
+                    messageError,
+                    "`Users.getAll` errors: "
+                        + String(describing: messageError)
+                )
+
+                expectations[0].fulfill()
+            }
+        )
+
+        wait(for: expectations, timeout: 60)
+    }
 }
